9ee50c2b80eab9a99d0136c93badc061bf4a3f74
Botrix <1486759736@qq.com>
2016-05-07 16:58:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/edl/Settings/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.edl.Settings;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.relatlayout);
+
+
+
+        //requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+
+        setContentView(R.layout.relatlayout);
+
+        //getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.actionbar);
+
+    }
+}
